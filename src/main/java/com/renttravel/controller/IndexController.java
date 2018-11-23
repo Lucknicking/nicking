51c57196f51fb1f53d3e0b70c4bce006cba72262
Lucknicking <1782854234@qq.com>
2018-11-23 15:21:55 +0800

    重构ionic框架

--- src/main/java/com/renttravel/controller/IndexController.java
+++ src/main/java/com/renttravel/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.renttravel.controller;
 
+import com.renttravel.dao.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
+        return "index";
+    }
+    @RequestMapping(value = "/api/login",method = RequestMethod.POST)
+    public String login(@RequestBody User user){
+        System.out.println(user);
         return "index";
     }
 }
