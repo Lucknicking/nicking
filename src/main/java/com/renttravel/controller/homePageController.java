59b392e2059e4d2bac100ba3f59b629c35637c45
Lucknicking <1782854234@qq.com>
2019-01-24 16:30:16 +0800

    后台修改，新增首页

---
+++ src/main/java/com/renttravel/controller/homePageController.java
@@ -0,0 +1,14 @@
+package com.renttravel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homePageController {
+
+    @GetMapping(value = "/index")
+    public String login(){
+        return "index";
+    }
+}
